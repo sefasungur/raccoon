b1edd0d32efa9e3cab57b856e73b3d363173c141
Sefa Sungur <info@sefasungur.com>
2015-04-14 15:53:26 +0300

    Config File
    Config File Updating

--- config/config.php
+++ config/config.php
@@ -1,8 +1,27 @@
 <?php
 
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Raccoon Open Source Content Managment System
+ * Version 1.0
+ * link https://github.com/sefasungur/raccoon
+ * Copyright 2015 Sefa Sungur <info@sefasungur.com>.
+ * @file config.php
+ * @time: 14.Nis.2015 - 15:46:01
+ * @author Sefa Sungur <info@sefasungur.com>
+ * @licence https://www.gnu.org/licenses/gpl-2.0.html
  */
 
+/* Database Settings */
+define ("DB_HOST","");  // Usually localhost
+define ("DB_USER","");  // Our Database Username
+define ("DB_NAME","");  // Our Database Name
+define ("DB_PASS","");  // Our Database Password
+define ("DB_PREF","");  // Our Database Prefix
+
+/* Timezone Setting */
+define ("DB_TIME","");  // Our location time zone. "Europe/Istanbul" etc. Please browse, http://php.net/manual/en/timezones.php
+
+/* Language Setting */
+define ("DB_LANG","");  // Our website's language. "tr" etc.
+
+?>

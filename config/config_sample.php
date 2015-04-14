3c155a0ec6008ddb7febb06f8d5b84573b5b3b2d
Sefa Sungur <info@sefasungur.com>
2015-04-14 15:56:25 +0300

    Config Sample File
    Add Config Sample File

---
+++ config/config_sample.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+ * Raccoon Open Source Content Managment System
+ * Version 1.0
+ * link https://github.com/sefasungur/raccoon
+ * Copyright 2015 Sefa Sungur <info@sefasungur.com>.
+ * @file config_sample.php
+ * @time: 14.Nis.2015 - 15:55:04
+ * @author Sefa Sungur <info@sefasungur.com>
+ * @licence https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/* Database Settings */
+define ("DB_HOST","localhost"); // Usually localhost
+define ("DB_USER","raccoon");   // Our Database Username
+define ("DB_NAME","raccoon");   // Our Database Name
+define ("DB_PASS","123456");    // Our Database Password
+define ("DB_PREF","rc");       // Our Database Prefix
+
+/* Timezone Setting */
+define ("DB_TIME","Europe/Istanbul");  // Our location time zone. "Europe/Istanbul" etc. Please browse, http://php.net/manual/en/timezones.php
+
+/* Language Setting */
+define ("DB_LANG","tr");  // Our website's language. "tr" etc.
+?>
